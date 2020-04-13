d8c9de33ad4223e26269a6cde3eed03fa6c1e7e3
AlecWebster1 <63371980+AlecWebster1@users.noreply.github.com>
2020-04-13 16:18:57 -0700

    Add files via upload

--- src/Encounter.java
+++ src/Encounter.java
@@ -10,51 +10,19 @@
  */
 import java.util.ArrayList;
 import java.lang.Math;
-public class Encounter {
-    
-    private ArrayList<String> MonsterList;
-    private ArrayList<String> SettingList;
-    
-    public Encounter()
-    {
-        MonsterList = new ArrayList();
-        MonsterList.add("Ogre");
-        MonsterList.add("Bat");
-        MonsterList.add("Vampire");
-        MonsterList.add("Werewolf");
-        MonsterList.add("Zombie");
-        MonsterList.add("Bear");
-        MonsterList.add("Giant Bug");
-        MonsterList.add("Giant");
-        MonsterList.add("Bandit");
-        MonsterList.add("Venus Fly Trap");
-        MonsterList.add("Slime");
-        MonsterList.add("Skeleton");
-        MonsterList.add("Giant Eyeball");
-        MonsterList.add("Witch");
-        MonsterList.add("Troll");
-        
-        SettingList = new ArrayList();
-        SettingList.add("Desert");
-        SettingList.add("Forest");
-        SettingList.add("Abandoned Mineshaft");
-        
-    }
-    
-    public String RandomMonster()
-    {
-        int random; 
-        random = (int)(Math.random()*15);
-        return MonsterList.get(random);
-    }
-    
-    public String Setting()
-    {
-        
-    }
-    
-    public 
-    
-    
-    
-}
+public interface Encounter 
+{
+
+   public Encounter getLeftChild();
+   public void setLeftChild(Encounter E);
+   
+   public Encounter getRightChild();
+   public void setRightChild(Encounter E);
+   
+   public Encounter getParent();
+   public void setParent(Encounter E);
+   
+   
+           
+
+   }
