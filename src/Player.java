7389075bc0d3022e7b7b8104d9f31237db3b7abe
DylanMcIntosh7 <56946135+DylanMcIntosh7@users.noreply.github.com>
2020-04-13 15:57:42 -0700

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,54 @@
+public class Player
+{
+    int currentHealth;
+    int maxHealth;
+    
+    
+    public Player()
+    {
+        maxHealth = 5;
+        currentHealth = 5;
+    }
+    
+    public int decrementHealth() //0 means player is dead, 1 means they successfully lost health
+    {
+        if(currentHealth == 1)
+        {
+            currentHealth--;
+            return 0;
+        }
+        else
+        {
+            currentHealth--;
+            return 1;
+        }
+    }
+    
+    public int incrementHealth() //0 means player is at max health, 1 means they successfully gained health
+    {
+        if(currentHealth == maxHealth)
+        {
+            return 0;
+        }
+        else
+        {
+            currentHealth++;
+            return 1;
+        }
+    }
+    
+    public int getCurrentHealth()
+    {
+        return currentHealth;
+    }
+    
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+    
+    public String toString()
+    {
+        return "You have " + currentHealth + " out of " + maxHealth + " hearts";
+    }
+}
