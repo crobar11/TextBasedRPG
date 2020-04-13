92fb14602f7fd483d3db5870f0d0b2a7fb79b48e
crobar11 <55898694+crobar11@users.noreply.github.com>
2020-04-13 16:04:58 -0700

    Add files via upload

---
+++ src/GameStatus.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+public class GameStatus<Encounter> 
+{
+    private BinaryTree tree;
+    ArrayList MonsterList;
+    Node currentNode;
+    
+    public GameStatus()
+    {
+        MonsterList = new ArrayList();
+        MonsterList.add("Ogre");
+        MonsterList.add("Bat");
+        MonsterList.add("Vampire");
+        MonsterList.add("Werewolf");
+        MonsterList.add("Zombie");
+        MonsterList.add("Bear");
+        MonsterList.add("Giant Bug");
+        MonsterList.add("Giant");
+        MonsterList.add("Bandit");
+        MonsterList.add("Venus Fly Trap");
+        MonsterList.add("Slime");
+        MonsterList.add("Skeleton");
+        MonsterList.add("Giant Eyeball");
+        MonsterList.add("Witch");
+        MonsterList.add("Troll");
+        
+    }
+    
+    public static void main(int [] args)
+    {
+        
+    }
+}
