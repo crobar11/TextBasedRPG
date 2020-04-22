23ddb8d93f9b722f472a2ccd0d61bf7190c4eed4
crobar11 <55898694+crobar11@users.noreply.github.com>
2020-04-22 14:41:29 -0700

    Add files via upload

---
+++ src/Potion.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+
+public class Potion extends Item
+{
+    private String name;
+    private int heal, strength, speed;
+    private ArrayList potions;
+    
+    public Potion(String n, int h, int st)
+    {
+        name = n;
+        heal = h;
+        strength = st;
+        
+    }
+    
+    public String getValue()
+    {
+        return "potion";
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void use()
+    {
+        
+    }
+    
+    public int getStrength()
+    {
+        return strength;
+    }
+    
+    public int getHeal()
+    {
+        return heal;
+    }
+    
+    public int getSpeed()
+    {
+        return speed;
+    }
+}
