854fdc3b32f9e6fc59ac8794f822746d4dcc89c0
crobar11 <55898694+crobar11@users.noreply.github.com>
2020-04-22 15:12:29 -0700

    Add files via upload

---
+++ src/GameStatus.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GameStatus
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome Adventurer, To TextQuest! \n Would You Like To Begin Your Quest? \n Type: \"Yes\" to continue");
+        String name = sc.nextLine();
+        if(name.equals("Yes") || name.equals("yes"))
+        {
+            System.out.println("SICK");
+        }
+        
+        
+        
+    }
+}
