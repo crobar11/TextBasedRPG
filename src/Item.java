53abc2c2df41c7936e63682bb3857a0d85f60a5c
crobar11 <55898694+crobar11@users.noreply.github.com>
2020-04-27 14:04:53 -0700

    Add files via upload

---
+++ src/Item.java
@@ -0,0 +1,7 @@
+public abstract class Item 
+{
+    //strength speed health
+    public abstract String getValue(); //return type of item
+    public abstract void use(); //what happens on use
+    public abstract String getName();//return name
+}
