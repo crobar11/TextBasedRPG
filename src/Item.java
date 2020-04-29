45e9867c31bae5d57cdecd001043512524020a25
nkarter02 <63371667+nkarter02@users.noreply.github.com>
2020-04-29 14:16:11 -0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,5 @@
-public interface Item 
+public abstract class Item 
 {
-    public String getValue(); //name
-    public int getKey(); //int
+    public abstract void use(); //what happens on use
+    public abstract String getName();//return name
 }
